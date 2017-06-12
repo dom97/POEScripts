dee9ce43acf3edd9cb4ff16545a5ad2f4f0a5621
dom97 <dom97@vt.edu>
2017-06-12 15:41:48 -0400

    Create PriceCategory.py

---
+++ PriceCategory.py
@@ -0,0 +1,26 @@
+#! python3
+# POEPriceCheck.py - checks price of item in poe
+# works with prophecies, divination cards, unique maps, flasks, weapons, armours, 
+#TODO get it to hit the show all
+import selenium, sys
+
+from selenium import webdriver
+
+categories = {'prophecy':'prophecies', 'div':'divinationcards', 'map':'unique-maps', 'jewel':'unique-jewels', 'flask':'unique-flasks', 'weapon':'unique-weapons', 'armour':'unique-armours', 'accessory':'unique-accessories'}
+end_url = categories[sys.argv[1]]
+
+browser = webdriver.Firefox()
+browser.get('http://poe.ninja/challenge/' + end_url)
+
+path = //*[contains(text(),'Show all')]
+
+browser.find_element_by_xpath(path).click()
+
+rows = browser.find_elements_by_tag_name('tr')
+del row[0]
+
+for row in rows
+    #here is a good place to parse each row and only print out the name and price in chaos
+    #probably the easiest way to do this is to split by space and grab all text prior to "wiki"
+    # to grab price either count back spaces in the split or find the span without style in the class "text-right" 
+    print(row.text)
