8f57d47977cb4845972abf039a5a03aef8ed11b8
dom97 <dom97@vt.edu>
2017-06-15 10:38:16 -0400

    add parsing of row data

--- PriceCategory.py
+++ PriceCategory.py
@@ -24,3 +24,14 @@ for row in rows
     #probably the easiest way to do this is to split by space and grab all text prior to "wiki"
     # to grab price either count back spaces in the split or find the span without style in the class "text-right" 
     print(row.text)
+    parts = row.split(' ')
+    index = -1
+    price_index = -1
+    for idx, part in enumerate(parts):
+        if part.lower() == 'wiki':
+    	    index = idx
+        elif part.lower() == 'x' and idx == (len(parts) - 1):
+            price_index = idx - 1
+        item_name = ' '.join(parts[0:index])
+        price = parts[price_index]
+        print(item_name + price)
