b8946e1e99ea35a31e0c944e817b51441cdc5eb5
Dom Edwards <dedwards@qualtrax.com>
2017-07-15 16:09:14 -0400

    added db suport

--- PriceCategory.py
+++ PriceCategory.py
@@ -5,23 +5,31 @@
 import selenium, sys
 
 from selenium import webdriver
+from tinydb import TinyDB, Query
 
+db = TinyDB('db.json')
+#probably flip these to name the db table
 categories = {'prophecy':'prophecies', 'div':'divinationcards', 'map':'unique-maps', 'jewel':'unique-jewels', 'flask':'unique-flasks', 'weapon':'unique-weapons', 'armour':'unique-armours', 'accessory':'unique-accessories'}
-end_url = categories[sys.argv[1]]
-
+urls = ['prophecies', 'divinationcards', 'unique-maps', 'unique-jewels', 'unique-flasks', 'unique-weapons', 'unique-armours', 'unique-accessories']
 browser = webdriver.Firefox()
-browser.get('http://poe.ninja/challenge/' + end_url)
+for url in urls:
+    table = db.table(url)
+    
+    browser.get('http://poe.ninja/challenge/' + url)
 
-path = "//*[contains(text(),'Show all')]"
+    path = "//*[contains(text(),'Show all')]"
+    try:
+        browser.find_element_by_xpath(path).click()
+    except(Exception):
+        pass
 
-browser.find_element_by_xpath(path).click()
+    rows = browser.find_elements_by_tag_name('tr')
+    del rows[0]
 
-rows = browser.find_elements_by_tag_name('tr')
-del rows[0]
-
-for row in rows:
-    parts = row.text.splitlines()
-    item_name = parts[0]
-    chaos_price_index = len(parts)-2
-    print(parts[0] + ':' + parts[chaos_price_index])
+    for row in rows:
+        parts = row.text.splitlines()
+        item_name = parts[0]
+        chaos_price = parts[len(parts)-2]
+        table.insert({'name': item_name, 'price': chaos_price})
+        #print(item_name + ':' + chaos_price)
 browser.close()
